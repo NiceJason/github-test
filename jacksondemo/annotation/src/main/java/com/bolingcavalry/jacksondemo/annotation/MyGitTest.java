cd5e698a72135addc153aefc0d7c5c7b3fcc823b
diaojianbin <diaojianbin@listener.com.cn>
2021-01-28 14:27:20 +0800

    第A次提交

---
+++ jacksondemo/annotation/src/main/java/com/bolingcavalry/jacksondemo/annotation/MyGitTest.java
@@ -0,0 +1,12 @@
+package com.bolingcavalry.jacksondemo.annotation;
+
+/**
+ * @Author DiaoJianBin
+ * @Date 2021/1/28 14:26
+ */
+public class MyGitTest {
+
+    public void gitTest(){
+        System.out.println("第A次提交");
+    }
+}
